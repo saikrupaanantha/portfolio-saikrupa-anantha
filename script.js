8ad7311f8610d97401f52a0226f8a042a883003a
saikrupaanantha <136228603+saikrupaanantha@users.noreply.github.com>
2025-11-29 16:55:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Script to toggle the navigation menu on small screens
+document.getElementById('nav-toggle').addEventListener('click', function () {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+});
+
+// Script to scroll to the top of the page
+var scrollToTopBtn = document.getElementById("scroll-to-top");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+    // Show the button when the user scrolls down 100px from the top of the document
+    if (rootElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    // Scroll smoothly to the top
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
+
